1262d549dac0269f824113b4e6bcd1ab941448fc
mbringas <maubringas@gmail.com>
2022-01-05 12:42:40 -0300

    remove some excessive prints

--- fundistribun.py
+++ fundistribun.py
@@ -2,7 +2,6 @@ import mdtraj as mt
 import numpy as np
 import os
 import csv
-#import matplotlib.pyplot as plt
 basename="apoCzrA"
 filenames=["apoCzrA.2.nc","apoCzrA.3.nc","apoCzrA.4.nc","apoCzrA.5.nc","apoCzrA.6.nc","apoCzrA.7.nc","apoCzrA.8.nc","apoCzrA.9.nc"]
 
@@ -26,14 +25,11 @@ for filename in filenames:
             hists[indice]=hists[indice]+chunkinfo[-1]*mt.compute_rdf(chunk, pairs, r_range=None, bin_width=0.005, n_bins=None, periodic=True, opt=True)[1]
     print("finish file "+filename)
 totchunks=sum(chunkinfo)
-print(totchunks)
 hist_scale=[]
 for hist in hists:
     hist_scale.append(hist/totchunks)
-print(hist_scale)
 
 printable=[list(xaxis)]+[list(x) for x in hist_scale]
-#with open('histograms.txt', 'a') as f1:
 with open("histograms.dat", "w", newline="") as f:
     writer = csv.writer(f,delimiter=' ', lineterminator='\n')
     writer.writerow(["#distance"]+["resid"+str(amides.index(amide)+1) for amide in amides])
